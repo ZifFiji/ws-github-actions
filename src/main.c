631b08a816956e1fbde26e014be8808fd666fa15
Arthur <arthur.guerinault@epitech.eu>
2024-03-13 10:28:42 +0100

    [ADD] - github actions

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
 
 int main(const int ac, char **av)
 {
-    if (ac != 2 || av == NULL || av[1] == NULL)
+    if (ac         != 2 || av == NULL || av[1] == NULL)
         return EPITECH_KO;
     return swapupcase(av[1]);
 }
