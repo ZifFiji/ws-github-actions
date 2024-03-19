71c155fa2c34f3df8cd73f0674d85f106936010f
Arthur <arthur.guerinault@epitech.eu>
2024-03-19 11:01:33 +0100

    [UPDATE] - test coding style

--- src/main.c
+++ src/main.c
@@ -10,7 +10,10 @@
 
 int main(const int ac, char **av)
 {
+    int c=0;
     if (ac != 2 || av == NULL || av[1] == NULL)
         return EPITECH_KO;
+    int a= 0;
+    int b = 4;
     return swapupcase(av[1]);
 }
